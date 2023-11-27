34245038a10473a5e30ebabbe6bc7352e70b45f6
Md Afzal Khan <138601073+khanAfzal17@users.noreply.github.com>
2023-11-27 11:02:46 +0530

    Update admin-index.js

--- src/scripts/admin-index.js
+++ src/scripts/admin-index.js
@@ -65,7 +65,6 @@ $(function () {
         })
     }
     $(document).on("click", "#btnAddNew", () => {
-        $("section").hide();
         LoadPage(`admin-add-video.html`);
         LoadCategories();
     })
